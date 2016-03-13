b6e459151842a4a334745c2d8e1e20567ce19f31
David Holbrook <davidbholbrook@gmail.com>
2016-03-12 20:36:32 -0800

    update test, add sparse actions test

--- test/test.js
+++ test/test.js
@@ -7,14 +7,14 @@ describe('decision-table-js', function() {
 
     var table = {
       conditions: [
-        [['A'],        ['true'], ['true'], ['false'],['false']],
-        [['B'],        ['true'], ['false'],['true'], ['false']]
+        ['A',        'true', 'true', 'false','false'],
+        ['B',        'true', 'false','true', 'false']
       ],
       actions: [
-        [['"Both"'],   ['true'], ['false'],['false'],['false']],
-        [['"Only A"'], ['false'],['true'], ['false'],['false']],
-        [['"Only B"'], ['false'],['false'],['true'], ['false']],
-        [['"Neither"'],['false'],['false'],['false'],['true']]
+        ['"Both"',   'true', 'false','false','false'],
+        ['"Only A"', 'false','true', 'false','false'],
+        ['"Only B"', 'false','false','true', 'false'],
+        ['"Neither"','false','false','false','true']
       ]
     };
 
@@ -42,6 +42,68 @@ describe('decision-table-js', function() {
     });
     assert.deepEqual(neither, ['Neither']);
 
+  });
+
+  it('should evaluate sparse actions and evaluate multipe actions', function() {
+
+    // from the wikipedia example https://en.wikipedia.org/wiki/Decision_table
+
+    var table = {
+      conditions: [
+        ['printer.notPrinting',                    'Y','Y','Y','Y','N','N','N','N'],
+        ['printer.redLightFlashing',               'Y','Y','N','N','Y','Y','N','N'],
+        ['printer.unrecognized',                   'Y','N','Y','N','Y','N','Y','N']
+      ],
+      actions: [
+        ['"Check the power cable"',                   ,   ,'X',   ,   ,   ,   ,   ],
+        ['"Check the printer-computer cable"',     'X',   ,'X',   ,   ,   ,   ,   ],
+        ['"Ensure printer software is installed"', 'X',   ,'X',   ,'X',   ,'X',   ],
+        ['"Check/replace ink"',                    'X','X',   ,   ,'X','X',   ,   ],
+        ['"Check for paper jam"',                     ,'X',   ,'X',   ,   ,   ,   ],
+      ]
+    };
+
+    var printingFlashingUnrecognized = decisionTable(table, {
+      Y: true, /* true alias */
+      N: false, /* false alias */
+      X: true, /* true alias */
+      printer: {
+        notPrinting: true,
+        redLightFlashing: true,
+        unrecognized: true
+      }
+    });
+    assert.deepEqual(printingFlashingUnrecognized,
+      ['Check the printer-computer cable',
+      'Ensure printer software is installed',
+      'Check/replace ink']);
+
+    var redLightFlashingUnrecognized = decisionTable(table, {
+      Y: true, /* true alias */
+      N: false, /* false alias */
+      X: true, /* true alias */
+      printer: {
+        notPrinting: false,
+        redLightFlashing: true,
+        unrecognized: true
+      }
+    });
+    assert.deepEqual(redLightFlashingUnrecognized,
+      ['Ensure printer software is installed',
+       'Check/replace ink']);
+
+    var noFlags = decisionTable(table, {
+      Y: false, /* true alias */
+      N: false, /* false alias */
+      X: false, /* true alias */
+      printer: {
+        notPrinting: false,
+        redLightFlashing: true,
+        unrecognized: true
+      }
+    });
+    assert.deepEqual(noFlags,
+     []);
   });
 
 });
